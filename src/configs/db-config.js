85f297726e7fcca190bbfcaba0e9b6530a411042
ianlevin01 <ianlevin01@gmail.com>
2025-07-28 23:33:07 -0300

    cambios

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -7,6 +7,8 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT) || 5432,
+  ssl: { rejectUnauthorized: false }, // obligatorio para Supabase
+  family: 4 // 👈 fuerza a usar IPv4
 };
 
 export default config;
