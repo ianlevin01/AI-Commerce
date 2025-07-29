9ed167ded416a3d88737bd3f6cdfae195458c2bc
ianlevin01 <ianlevin01@gmail.com>
2025-07-28 22:45:18 -0300

    prueba para render

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -64,6 +64,20 @@ router.post('/login', async (req, res) => {
 
     return respuesta;
 });
+
+router.get('/prueba', async (req, res) => {
+
+    let respuesta;
+    try {
+        respuesta = res.status(200).send('Funciono');
+
+    } catch (error) {
+        respuesta = res.status(500).send('Error interno.');
+    }
+
+    return respuesta;
+});
+
 router.get('/:id_user', async (req, res) => {
 
     let respuesta;
@@ -82,16 +96,5 @@ router.get('/:id_user', async (req, res) => {
 
     return respuesta;
 });
-router.get('/prueba', async (req, res) => {
 
-    let respuesta;
-    try {
-        respuesta = res.status(200).send('Funciono');
-
-    } catch (error) {
-        respuesta = res.status(500).send('Error interno.');
-    }
-
-    return respuesta;
-});
 export default router;
