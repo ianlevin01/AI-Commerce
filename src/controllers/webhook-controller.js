e6babffcb81f350b8ac32724288fb2bc641a425d
ianlevin01 <ianlevin01@gmail.com>
2025-07-30 21:41:19 -0300

    webhooks

--- src/controllers/webhook-controller.js
+++ src/controllers/webhook-controller.js
@@ -4,3 +4,41 @@ import axios from 'axios';
 
 const router = Router();
 const svc = new UserService();
+router.post('/store/redact', async (req, res) => {
+
+    let respuesta;
+    try {
+        respuesta = res.status(200).send('OK');
+
+    } catch (error) {
+        respuesta = res.status(500).send('Error interno.');
+    }
+
+    return respuesta;
+});
+router.post('/customers/redact', async (req, res) => {
+
+    let respuesta;
+    try {
+        respuesta = res.status(200).send('OK');
+
+    } catch (error) {
+        respuesta = res.status(500).send('Error interno.');
+    }
+
+    return respuesta;
+});
+router.post('/custome/redact', async (req, res) => {
+
+    let respuesta;
+    try {
+        respuesta = res.status(200).send('OK');
+
+    } catch (error) {
+        respuesta = res.status(500).send('Error interno.');
+    }
+
+    return respuesta;
+}); 
+
+export default router;
