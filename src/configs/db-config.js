7c5171cce9cfd37f96933259f659666ecb14d7ff
ianlevin01 <ianlevin01@gmail.com>
2025-07-31 18:58:31 -0300

    cambio para render

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -2,13 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT) || 5432,
-  ssl: { rejectUnauthorized: false }, // obligatorio para Supabase
-  family: 4 // 👈 fuerza a usar IPv4
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // obligatorio en Supabase
+  family: 4 // fuerza IPv4
 };
 
 export default config;
